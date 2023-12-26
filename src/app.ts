52c38bbc1354ee2820d041a19b650365739bb3f4
Roger Pantil <rogerpantilyowger@gmail.com>
2023-12-27 02:25:13 +0800

    temporary fix

--- src/app.ts
+++ src/app.ts
@@ -12,13 +12,14 @@ process.on("uncaughtException", (error) => {
 
 const app = express()
 const port = process.env.PORT || 3000
-const allowedOrigin = "https://yow-fast-fingers.onrender.com"
+// const allowedOrigin = "https://yow-fast-fingers.onrender.com"
 
 app.set("port", port)
 app.use(compression())
 app.use(express.json({ limit: "1kb" }))
 app.use(express.urlencoded({ limit: "1kb", extended: true }))
-app.use(cors({ origin: allowedOrigin, optionsSuccessStatus: 200 }))
+// app.use(cors({ origin: allowedOrigin, optionsSuccessStatus: 200 }))
+app.use(cors())
 
 app.use("/", routes)
 
